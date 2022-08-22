b53fe8e415f08446478997b4d07dc152dbb397c9
lad68 <1198699862@qq.com>
2022-08-22 21:43:05 +0800

    1.1员工登录与退出 1.2添加管理员

--- src/main/java/com/lixubo/reggie/ReggieApplication.java
+++ src/main/java/com/lixubo/reggie/ReggieApplication.java
@@ -3,9 +3,16 @@ package com.lixubo.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * @Slf4j 日志
+ * @SpringBootApplication 启动类
+ * @ServletComponentScan  扫描过滤器等注解
+ */
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lixubo/reggie/controller/EmployeeController.java
+++ src/main/java/com/lixubo/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 /**
  * @Author LIXUBO
@@ -29,12 +30,13 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
 
         //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
@@ -42,37 +44,65 @@ public class EmployeeController {
 
         //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
         //3、如果没有查询到则返回登录失败结果
-        if (emp == null){
+        if (emp == null) {
             return R.error("登陆失败");
         }
 
         //4、密码比对，如果不一致则返回登录失败结果
-        if (!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("登陆失败");
         }
 
         //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果if(emp.getStatus() == 0){
-        if (emp.getStatus() == 0){
+        if (emp.getStatus() == 0) {
             return R.error("账号已禁用");
         }
         //6、登录成功，将员工id存入Session并返回登录成功结果request.getSession().setAttribute("employee",emp.getId());
-        request.getSession().setAttribute("employee",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
     }
 
     /**
      * 员工退出
+     *
      * @param request
      * @return
      */
     @PostMapping("/logout")
-    public R<String> logout(HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         //清理Session中保存的当前登录的员工ID
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employee
+     * @return
+     */
+//保存员工信息
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+        //设置初始密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
+
     }
 }
