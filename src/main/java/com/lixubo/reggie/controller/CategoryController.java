a1968edadc5591b85459948abab0168e850f9c14
lad68 <1198699862@qq.com>
2022-08-23 20:56:06 +0800

    1.1员工登录与退出 1.2添加管理员 1.3员工信息分页查询 1.4公共字段自动填充
    2.1新增分类管理
    2.2分类信息分页查询

--- src/main/java/com/lixubo/reggie/controller/CategoryController.java
+++ src/main/java/com/lixubo/reggie/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package com.lixubo.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.lixubo.reggie.common.R;
+import com.lixubo.reggie.entity.Category;
 import com.lixubo.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author LIXUBO
@@ -19,4 +22,31 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> sava(@RequestBody Category category){
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        log.info("Page = {} , pageSize = {} ", page,pageSize);
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
+        //添加排序条件
+        queryWrapper.orderByAsc(Category::getSort);
+
+        //执行查询
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
 }
