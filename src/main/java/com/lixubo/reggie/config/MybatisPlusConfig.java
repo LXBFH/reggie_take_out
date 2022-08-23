0a8ce98079f83eb04453d4bacdf3808ee25dbac9
lad68 <1198699862@qq.com>
2022-08-23 12:23:18 +0800

    1.1员工登录与退出 1.2添加管理员 1.3员工信息分页查询

---
+++ src/main/java/com/lixubo/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.lixubo.reggie.config;
+
+/**
+ * @Author LIXUBO
+ * @Date 2022-08-23 11:43
+ * @description 分页插件配置类
+ * @Version 1.0
+ */
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP的分页插件
+ * 加入一个拦截器
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
