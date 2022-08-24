59a4fb8e32e2dc0d1154f5f30350d6155fcd60be
lad68 <1198699862@qq.com>
2022-08-24 14:56:02 +0800

    1.1员工登录与退出 1.2添加管理员 1.3员工信息分页查询 1.4公共字段自动填充
    2.1新增分类管理
    2.2分类信息分页查询
    2.3删除分类基础功能实现
    2.4完善删除分类
    2.5新增修改分类
    
    3.1.1 文件上传
    3.1.2 文件下载

--- src/main/java/com/lixubo/reggie/controller/CommonController.java
+++ src/main/java/com/lixubo/reggie/controller/CommonController.java
@@ -3,12 +3,16 @@ package com.lixubo.reggie.controller;
 import com.lixubo.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -47,7 +51,7 @@ public class CommonController {
         //创建目录
         File dir = new File(basePath);
         //判断当前目录是否存在，不存在就创建
-        if(!dir.exists()){
+        if (!dir.exists()) {
             //目录不存在，创建目录
             dir.mkdir();
         }
@@ -58,5 +62,37 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+
+        //输入流，通过输入流读取文件内容
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            //输出流，通过输出流将文件写回浏览器，在浏览器显示图片信息
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
